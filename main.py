d2ff44d91a8b02ba09b62f172f03eff08163f67f
Sarmad191387 <191387@students.au.edu.pk>
2023-02-19 17:23:40 +0500

    Pushing data to redis queue

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import model
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 queue_name = 'users'
 
-# creating db session
+# Creating db session
 def get_database_session():
     try:
         db = SessionLocal()
@@ -24,14 +24,6 @@ model.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
-
-
-# @app.post("/")
-# async def create_item(item: Item):
-#     with redis.Redis() as redis_client:
-#         redis_client.lpush("queue", 1)
-#     print(item.job_id)
-#     return item.job_id
 
 
 @app.post("/{job_id}")
@@ -53,35 +45,4 @@ async def read_item(job_id, request: Request, db:Session=Depends(get_database_se
     for x in redultList:
         redis_client.lpush(queue_name, x["user_id"])
     
-    # resultList = list(dict.values())
-    # print(resultList)
-
-
-# import os
-# import redis
-
-# # Establish a connection to Redis
-# r = redis.Redis(host='localhost', port=6379, db=0)
-
-# # Specify the name of the Redis queue to read from
-# queue_name = 'file_paths_queue'
-
-# # Create a loop to continuously read messages from the Redis queue
-# while True:
-#     # Read a message from the Redis queue
-#     message = r.brpop(queue_name)
-
-#     # If the queue is empty, break the loop
-#     if message is None:
-#         break
-
-#     # Get the path to the file from the message content
-#     file_path = message[1].decode('utf-8')
-
-#     # Read the contents of the file
-#     with open(file_path, 'r') as f:
-#         contents = f.read()
-
-#     # Process the contents of the file as needed
-#     # ...
-
+    
