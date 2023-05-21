dfb33750fb52d7590baaf606b24a42cca525ea5d
Sarmad191387 <191387@students.au.edu.pk>
2023-05-21 18:20:02 +0500

    new db

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:DummySQL786@python_db/python_db"
+DATABASE_URL = "mysql+mysqlconnector://root:DummySQL786@python_db/pythondb"
 
 engine = create_engine(DATABASE_URL)
 
